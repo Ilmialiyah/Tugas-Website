7d5bab471df5bdd7a7b9565abbd595753bde9fcb
Safa Auliya <56856328+safaauliyaa@users.noreply.github.com>
2020-12-12 01:32:47 +0700

    modal 1

---
+++ telusuri indonesia/Login.js
@@ -0,0 +1,36 @@
+let modal = document.querySelector('.addModal'); /* . untuk class */
+console.log(modal)
+let bg = document.querySelector('.bgModal');
+console.log(bg)
+let inputanUsername = document.querySelector('#username');/* # untuk id */
+console.log(inputanUsername)
+let inputanPassword = document.querySelector('#password');
+console.log(inputanPassword)
+let btnLogin = document.querySelector('input[type="button"]');
+console.log(btnLogin)
+let btnTutup = document.querySelector('.tutupModal');
+console.log(btnTutup)
+let modalHeading = document.querySelector('.bgModal');
+console.log(modalHeading)
+
+function showModal() {
+    modal.style.display = "flex";
+    bg.style.display = "flex";
+};
+
+function hideModal() {
+    modal.style.display = "none";
+    bg.style.display = "none";
+};
+
+function login() {
+    if (inputanUsername.value == 'username' && inputanPassword.value == 'password'){
+        showModal();
+    } else {
+        modalHeading.innerText = `Maaf username ${inputanUsername.value} tidak terdaftar`;
+        showModal();
+    }
+};
+
+btnLogin.addEventListener("click", login);
+btnTutup.addEventListener("click", hideModal);
